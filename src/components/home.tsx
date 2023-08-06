5890b11df9d671cfce0609a764f99970255fd2c3
Kennedy Muinde <kennedymuinde@gmail.com>
2023-08-06 22:33:57 +0300

    added 2nd section

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,20 +1,43 @@
 import bg from "../assets/bg.jpg";
 import Socials from "./socials";
+import pic from "../assets/me.jpeg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
 
 function Home() {
   return (
-    <div className="relative w-full h-screen bg-black">
+    <div className="relative w-full bg-black">
       <div
         className="fixed top-0 left-0 w-full h-full bg-cover bg-center"
         style={{ backgroundImage: `url(${bg})`, opacity: "0.2" }}
       ></div>
-      <div className="relative z-10 text-center text-white p-20">
-        <div className="mt-20">
+      <div className="relative z-10 text-center text-white mt-5">
+        <div className="h-[89vh] pt-[15rem]">
           <h1 className="text-[8vw] font-light">HI, I'M KENNEDY</h1>
           <p className="mt-2 tracking-[0.52rem]">Fullstack Developer</p>
-          {/* <div className="left my-5 md:my-0 bg-white p-20">MY PICTURE</div> */}
+          <div className="text-3xl mt-[30vh] hover:rotate-180 transition-all duration-500">
+            <FontAwesomeIcon icon={faArrowDown} />
+          </div>
         </div>
-        <div>{/* <Socials /> */}</div>
+        <div className="bg-white p-10 text-gray-800">
+          <div className="flex justify-center">
+            <img src={pic} alt="" className="w-[15rem] mb-10 rounded-full" />
+          </div>
+          <p className="">
+            Hello, I'm Kennedy, a passionate and versatile fullstack developer
+            dedicated to crafting seamless digital experiences. With a blend of
+            creativity and technical expertise, I bring ideas to life through
+            elegant and functional web applications. My journey in the world of
+            development has equipped me with a diverse skill set, ranging from
+            front-end aesthetics to back-end efficiency. Through a commitment to
+            continuous learning and innovation, I strive to deliver solutions
+            that not only meet but exceed expectations. Join me as I embark on
+            this coding adventure, where every line of code is an opportunity to
+            make the virtual world a better place. Welcome to my portfolio,
+            where innovation meets execution.
+          </p>
+          <Socials />
+        </div>
       </div>
     </div>
   );
