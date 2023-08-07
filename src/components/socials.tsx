4beed868850977f3f0129cfe570505f4e9340e60
Kennedy Muinde <kennedymuinde@gmail.com>
2023-08-07 09:23:11 +0300

    changed github link

--- src/components/socials.tsx
+++ src/components/socials.tsx
@@ -4,7 +4,7 @@ function Socials() {
       <div className="mt-20">
         <ul className="flex justify-center md:space-x-5 ">
           <li className="bg-orange-500 p-2 m-2 rounded-md hover:bg-yellow-400 hover:m-0 hover:p-4 transition-all duration-300">
-            <a href="https://github.com/" target="blank">
+            <a href="https://github.com/Volatyl" target="blank">
               GITHUB
             </a>
           </li>
